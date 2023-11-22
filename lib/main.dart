55da6c4ab2b4205bcd35b41114a6696b8ab0d162
shelly <145767382+shllie@users.noreply.github.com>
2023-11-22 12:28:25 +0100

    AndroidManifest & const

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'homescreen.dart';
 import 'screen2.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RPGpickr',
       initialRoute: '/screen1',
       routes: {
-        '/screen1': (context) => Screen1(),
+        '/screen1': (context) => const Screen1(),
         '/screen2': (context) => Screen2(),
         // Für neue Routen
       },
