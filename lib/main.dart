6d382c8b7b8a848a9678954e66032769849747d8
shelly <145767382+shllie@users.noreply.github.com>
2023-12-19 11:19:16 +0100

    // loginscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rpgpickr/firebase_options.dart';
 import 'homescreen.dart';
 import 'screen2.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+  } catch (e) {
+    print("Failed to initialize Firebase: $e");
+  }
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
